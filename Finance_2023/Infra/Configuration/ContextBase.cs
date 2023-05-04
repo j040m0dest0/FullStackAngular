6d4e282a37f26fe0d2719490236d84743e7f469c
joaom <joaom@DESKTOP-DT2GM4U>
2023-05-04 16:55:17 -0300

    add infra content

---
+++ Finance_2023/Infra/Configuration/ContextBase.cs
@@ -0,0 +1,50 @@
+﻿using Entities.Entiti;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options): base(options) 
+        {
+
+
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiros { get; set; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiros { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Despesa> Despesas { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=localhost;Initial Catalog=API_DDD_2023;Integrated Security=True";
+        }
+
+
+    }
+}
